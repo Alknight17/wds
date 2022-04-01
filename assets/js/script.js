ae97053e52bd1e1241e9565f11f41fa5ced67842
Alknight17 <knightalex17@gmail.com>
2022-04-01 18:04:12 -0400

    display date on page/psuedo code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
 // find current day/time
-let d;
-d = new Date();
-console.log(d.toString());
+var timeDisplayEl = document.querySelector("#time-display");
+timeDisplayEl.textContent = moment().format("dddd MMMM Mo YYYY");
+
+// assign the correct time vlue to each time block
+// add or remove classes based on past/present/future
+// add a save button that saves text in each block
