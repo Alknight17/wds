3b899b405af3483277ae88904527b6cc3f565012
Alknight17 <knightalex17@gmail.com>
2022-04-04 14:36:40 -0400

    reformat time-blocks to look better

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,17 @@
 var timeDisplayEl = document.querySelector("#time-display");
 timeDisplayEl.textContent = moment().format("dddd MMMM Mo YYYY");
 
+// variable to selecting each timeblock and add classes 
+var timeBlock = document.getElementById('textarea');
+
+// save tasks to local storage
+const saveBtn = document.querySelector('#save');
+const getTask = document.querySelector('.textarea');
+
+saveBtn.addEventListener('click', function () {
+  localStorage.setItem('task', getTask.value);
+});
+
 // global variable for current hour
 var now = moment();
 var currentHour = now.hour();
@@ -13,14 +24,19 @@ var checkHour = function() {
       var id = $(this).attr('id');
       var splitId =  id.split("-");
       var elementHour = parseInt(splitId[1]);
-
-      if (elementHour < currentHour ) {
-
-      } else if (elementHour === currentHour) {
-
-      } else if (elementHour > currentHour) {
-
+      console.log(elementHour);
+      
+      if (currentHour > elementHour) {
+        timeBlock.classList.add("past");
+      
+      } else if (currentHour === elementHour) {
+        timeBlock.classList.add("present");
+      
+      } else if (currentHour < elementHour) {
+        timeBlock.classList.add("future");
       }
     });
 }
+
+checkHour();
 
